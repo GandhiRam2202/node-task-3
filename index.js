a1b11ef64c2d690cc2b915996c69c14723f2f438
GandhiRam2202 <gandhiramparthi@gmail.com>
2024-03-20 04:39:00 +0530

    update

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 import express from 'express';
 import ConnectDB from './DBConnect/DBConnect.js';
-import cors from 'cors'
+
 import router from './Router/Router.js';
 import dotenv from 'dotenv'
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 dotenv.config();
 ConnectDB();
 const port = process.env.PORT;
 
-app.get('/', (req, res) => {
-  res.status(200).json({"message": 'Welcome to the Nodejs file system'});
-})
+
 
 
 app.use('/api',router);
